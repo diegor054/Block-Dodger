274c13769f8ae8535d509dd464d1c27635f432a5
diegor054 <thebrickwall054@gmail.com>
2021-11-20 17:31:59 -0800

    demo 1

--- source/main.c
+++ source/main.c
@@ -22,14 +22,15 @@ typedef struct task {
 } task; 
 
 
-task tasks[1]; 
-const unsigned long tasksPeriodGCD = 200; 
-const unsigned long LED = 200;
-const unsigned long roundPeriod = 200;
+task tasks[3]; 
+const unsigned long tasksPeriodGCD = 100; 
+const unsigned long LED = 500;
+const unsigned long roundPeriod = 500;
+const unsigned long LED2 = 300;
 
 void TimerISR() {
 	unsigned char i; 
-	for(i = 0; i < 2; ++i){ 
+	for(i = 0; i < 3; ++i){ 
         if(tasks[i].elapsedTime >= tasks[i].period){         
 	tasks[i].state = tasks[i].TickFct(tasks[i].state);            
 	tasks[i].elapsedTime = 0; 
@@ -42,8 +43,12 @@ unsigned char row[20] = {8, 7,6,5,4,3,2,1,0,0,0,0,8,7,6,5,4,3,2,1};
 unsigned char column[16] = {0,2,0,7,0,4,2,4,2,7,3,5,3,4,6,7};
 unsigned char r = 0;
 unsigned char c = 0;
+unsigned char round1 = 0;
+unsigned char round2 = 0;
+
 enum LED_States{LED_start, LED_display} LED_state;
 int LED_Tick(int state){
+	if(round1){
 	unsigned char tmpD = 0x00;
 	unsigned char tmpC = 0x00;
 	switch(LED_state){
@@ -104,24 +109,120 @@ int LED_Tick(int state){
 	LED_state = LED_display;
 	break;
 }
+}
 	return state;
 }
 
+unsigned char row2[20] = {8, 7,6,5,4,3,2,1,0,0,0,0,8,7,6,5,4,3,2,1};
+unsigned char column2[8] = {0,2,7,4,2,3,5,7};
+unsigned char r2 = 0;
+unsigned char c2 = 0;
+enum LED2_States{LED2_start, LED2_display} LED2_state;
+int LED2_Tick(int state){
+	if(round2){
+	unsigned char tmpD = 0x00;
+	unsigned char tmpC = 0x00;
+	switch(LED2_state){
+	case LED2_start:  //set the board completely off initially
+	tmpD = tmpD | 0x24;
+	PORTD = tmpD;
+	for(int i = 0; i < 8; i++){
+	tmpD = (tmpD & 0xF6);
+	tmpC = (tmpC & 0xFE);
+	PORTC = tmpC;
+	PORTD = tmpD;
+	tmpD = tmpD | 0x09;
+	tmpC = tmpC | 0x01;
+	PORTC = tmpC;
+	PORTD = tmpD;
+	}
+	tmpD = tmpD | 0x12;
+	tmpC = tmpC | 0x02;
+	PORTC = tmpC;
+	PORTD = tmpD;
+	LED2_state = LED2_display;
+	break;
+	
+	case LED2_display:
+	tmpC = 0x00;
+	tmpD = 0x00;   //D5 should be 0, (SER)
+	PORTD = tmpD;
+
+	for(int m = 0; m < 9; m++){
+	if(m == row2[r2] || m == row2[r2 + 10]){
+	tmpC = tmpC | 0x04; //make C2 1, (SER shift 1 in) 
+	}
+	PORTC = tmpC;
+	
+	tmpC = (tmpC & 0xFE);  //make C0 0(SRCLK)
+	
+	for(int n = 0; n < 8; n++){
+	tmpD = (tmpD & 0xD7);  //make D3 0 (SRCLK)
+	if(n == column2[c2]){ 
+	tmpD = tmpD | 0x20; //D5 to 1 SER
+	}
+	PORTD = tmpD;
+	tmpD = tmpD | 0x08;    //make D3 1 (SRCLK) red row
+	PORTD = tmpD;
+	}
+	
+	PORTC = tmpC;
+	tmpC = tmpC | 0x01;    //make C0 1 (SRCLK) column
+	PORTC = tmpC;
+	tmpC = tmpC & 0xFB;    //make C2 0 (SER)
+	PORTC = tmpC; //
+	}
+
+	tmpC = tmpC | 0x02;    //make C1 1 to output it (RCLK) 
+	tmpD = tmpD | 0x10;    //make D4 1 to output it (RCLK)
+	PORTC = tmpC;
+	PORTD = tmpD;
+	LED2_state = LED2_display;
+	break;
+}
+}
+	return state;
+}
+
+int counter = 0;
 enum Round_States{round_start, round_inc} r_state;
 int Round_Tick(int state){
 	switch(r_state){
 	case round_start:
 	r_state = round_inc;
+	round1 = 1;
+	round2 = 0;
 	break;	
 	
 	case round_inc:
+	if(round1){
 	r++;
-	if(r >= 10){
+	if(r >= 10 && counter <=2){
 	r = 0;
+	counter++;
 	c+= 2;
 	}
 	if(c >= 16){
 	c = 0;
+	}
+	if(counter == 3){
+	counter = 0;
+	round1 = 0;
+	round2 = 1;
+	r = 0;
+	c = 0;
+	}
+	}
+
+	if(round2){
+	r2++;
+	if(r2 >= 10){
+	r2 = 0;
+	c2++;
+	}
+	if(c2 >= 8){
+	c2 = 0;
+	}
 	}
 	r_state = round_inc;
 	break;
@@ -145,6 +246,11 @@ int main(void) {
     	tasks[i].period = roundPeriod; 
     	tasks[i].elapsedTime = tasks[i].period; 
    	tasks[i].TickFct = &Round_Tick; 
+	i++;
+	tasks[i].state = LED2_start; 
+    	tasks[i].period = LED2; 
+    	tasks[i].elapsedTime = tasks[i].period; 
+   	tasks[i].TickFct = &LED2_Tick; 
 	i++;
     	TimerSet(tasksPeriodGCD); 
     	TimerOn();
